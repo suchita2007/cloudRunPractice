287e8c417a8ab16a49b23c5a21c0fa9815315260
suchita2007 <72294646+suchita2007@users.noreply.github.com>
2022-01-21 16:30:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const PORT = process.env.PORT || 80
 http
   .createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello Everyone, Welcome to Google Cloud')
+    res.end('Hello Everyone, Welcome to Google Cloud.................suchita and vanatan well come to MediaAgility')
   })
   .listen(PORT, () => console.log('Listening on', PORT))
